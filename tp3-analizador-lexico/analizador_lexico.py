00c58474d370b25c4b7ea097811eca74642b27a3
Aldana Vega <aldanavega14@gmail.com>
2026-04-22 19:19:27 -0300

    feat: mejora en el manejo de errores con manejo de columnas y errores detallados

--- tp3-analizador-lexico/analizador_lexico.py
+++ tp3-analizador-lexico/analizador_lexico.py
@@ -4,6 +4,7 @@ def analyzer(source):
     i = 0
     n = len(source)
     line_number = 1
+    col = 1
 
     while i < n:
         c = source[i]
@@ -13,6 +14,9 @@ def analyzer(source):
             i += 1
             if(c == '\n'):
                 line_number += 1
+                col = 1
+            else:
+                col += 1
             continue
 
         # analizar símbolos de un solo caracter o compuestos
@@ -21,74 +25,91 @@ def analyzer(source):
                 if i + 1 < n and source[i + 1] == '=':
                     tokens.append(("ASIGNAR", ":="));
                     i += 2;
+                    col += 2;
                 else:
                     tokens.append(("dos_puntos", "DOS_PUNTOS"));
                     i += 1;
+                    col += 1;
             case '<':
                 if i + 1 < n and source[i + 1] == '>':
                     tokens.append(("op_relacional", "NE"));
                     i += 2;
+                    col += 2;
                 elif i + 1 < n and source[i + 1] == '=':
                     tokens.append(("op_relacional", "LE"));
                     i += 2;
+                    col += 2;
                 else:
                     tokens.append(("op_relacional", "LT"));
                     i += 1;
+                    col += 1;
             case '>':
                 if i + 1 < n and source[i + 1] == '=':
                     tokens.append(("op_relacional", "GE"));
                     i += 2;
+                    col += 2;
                 else:
                     tokens.append(("op_relacional", "GT"));
                     i += 1;
+                    col += 1;
             case '=':
                 tokens.append(("op_relacional", "EQ"));
                 i += 1;
+                col += 1;
             case '+':
                 tokens.append(("op_aritmetico", "SUMA"));
                 i += 1;
+                col += 1;
             case '-':
                 tokens.append(("op_aritmetico", "RESTA"));
                 i += 1;
+                col += 1;
             case '*':
                 tokens.append(("op_aritmetico", "MUL"));
                 i += 1;
+                col += 1;
             case '/':
                 tokens.append(("op_aritmetico", "DIV"));
                 i += 1;
+                col += 1;
             case ';':
                 tokens.append(("punto_coma", "PUNTO_COMA"));
                 i += 1;
+                col += 1;
             case ',':
                 tokens.append(("coma", "COMA"));
                 i += 1;
+                col += 1;
             case '.':
                 tokens.append(("punto", "PUNTO"));
                 i += 1;
+                col += 1;
             case '(':
                 tokens.append(("paren_izq", "PAREN_IZQ"));
                 i += 1;
+                col += 1;
             case ')':
                 tokens.append(("paren_der", "PAREN_DER"));
                 i += 1;
-            case '{': 
+                col += 1;
+            case '{':
                 end = i + 1;
                 while end < n and source[end] != '}':
                     end += 1;
+                    col += 1;
                 if end < n:
                     i = end + 1;
                 else:
-                    print(f"Error léxico en línea {line_number}");
-                    i += 1;
+                    print(f"Error léxico en línea {line_number}, comentario sin cerrar");
             # analizar palabras (identificadores y palabras reservadas) o números
             case _:
                 if c.isalpha():
                     start = i
                     while i < n and (source[i].isalnum() or source[i] == '_'):
                         i += 1
-                    
+                        col += 1
+
                     word = source[start:i]
-                    
                     if word.lower() in keyword_table:
                         tokens.append((word.upper(), word))
                     else:
@@ -97,10 +118,12 @@ def analyzer(source):
                     start = i
                     while i < n and source[i].isdigit():
                         i += 1
+                        col += 1
                     tokens.append(("num", source[start:i]))
                 else:
                     i += 1;
-                    print(f"Error léxico en línea {line_number}")
+                    print(f"Error léxico en línea {line_number}, col {col}, caracter no válido: {c}")
+                    col += 1;
     return tokens
 
 def read_source(fileName):
